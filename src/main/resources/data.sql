1a63eec8fd5395f35235b052f492754bc59de359
“Oleh <nagornyak68@gmail.com>
2022-11-25 02:30:29 +0200

    performed sql script

---
+++ src/main/resources/data.sql
@@ -0,0 +1,23 @@
+-- -----------------------------------------------------
+-- Data for table customers
+-- -----------------------------------------------------
+
+insert into customers (id,email,first_name,last_name,phone,address_line) values (default,'alex@gmail.com','Alex','Smith','(066) 666-6666','st. Horodotska, 38');
+
+-- -----------------------------------------------------
+-- Data for table ingredients
+-- -----------------------------------------------------
+
+insert into ingredients (id,name,price) values (default,'Bechamel sauce',35);
+insert into ingredients (id,name,price) values (default,'Mozzarella',60);
+insert into ingredients (id,name,price) values (default,'Chicken',45);
+
+-- -----------------------------------------------------
+-- Data for table pizza
+-- -----------------------------------------------------
+
+insert into pizza (id,name) values (default,'Italiana');
+
+insert into pizza_ingredients (pizza_id,ingredients_id) values (1,1);
+insert into pizza_ingredients (pizza_id,ingredients_id) values (1,2);
+
